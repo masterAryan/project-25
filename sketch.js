dab78aaf0582f18279a76ccf5997b5bf5ca30c88
masterAryan <66153461+masterAryan@users.noreply.github.com>
2020-07-18 06:58:02 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var box,box1;
+var ground;
+var ball;
+var dbimg,db;
+
+function preload()
+{
+	dbimg = loadImage("dustbingreen.png");
+	paperimg = loadImage("paper.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    ground = new Ground(width/2,650,800,50)
+	box = new Box(width/1.45,580,10,100)
+	box1 = new Box(width/1.25,580,10,100)
+	Engine.run(engine);
+	
+  ball = new Ball(50,60,10);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(246,36,255);
+  ground.display();
+  box.display();
+  box1.display();
+  ball.display();
+  drawSprites();
+  image(dbimg,550,525,100,100);
+  
+  
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(ball.body,ball.body.position,{x:18,y:-12});
+
+	}
+}
+
+
+
+
